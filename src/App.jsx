e2a1c8b1d08426574cdc6dfaea39421ef1616570
Balu <kharadeb999@gmail.com>
2025-12-13 18:47:01 +0530

    css-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
+import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <h1>Dev Tinder</h1>
+      <Navbar />
     </>
   );
 }
